aa0f061cc2d607fa27627097c68e387aeb60be6d
Chris Yuen <chris@kizzx2.com>
2020-09-14 10:23:31 +0800

    tag: 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sbe",
-    version="0.2.3",
+    version="0.2.4",
     author="Chris Yuen",
     author_email="chris@kizzx2.com",
     long_description=open('README.md', 'r').read(),

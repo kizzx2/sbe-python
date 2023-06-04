6653ea575d3ecee752fb97afd7b9651cf0ea0b6d
Chris Yuen <chris@kizzx2.com>
2023-06-04 23:49:43 +0800

    0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sbe",
-    version="0.3.2",
+    version="0.3.3",
     author="Chris Yuen",
     author_email="chris@kizzx2.com",
     long_description=open('README.md', 'r').read(),
